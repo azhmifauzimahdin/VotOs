499c3b726a6d0369388532a33028cfed44cecb1a
Azhmi Fauzi Mahdin <azhmifauzi11@gmail.com>
2023-03-26 17:52:24 +0700

    [4]Blade Templating Engine

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,58 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home', [
+        "title" => "Home",
         "name" => "Azhmi Fauzi Mahdin",
         "email" => "azhmifauzi11@gmail.com"
+    ]);
+});
+
+
+Route::get('/about', function () {
+    $blog_post = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Azhmi Fauzi Mahdin",
+            "body" => "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit voluptatum repudiandae sequi fuga at? Voluptatum ullam voluptates facilis iste, iusto, molestiae quis illo, adipisci rem impedit nostrum saepe hic? Deleniti!"
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => 'judul-post-kedua',
+            "author" => "Airlangga",
+            "body" => "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit voluptatum repudiandae sequi fuga at? Voluptatum ullam voluptates facilis iste, iusto, molestiae quis illo, adipisci rem impedit nostrum saepe hic? Deleniti!"
+        ]
+    ];
+    return view('about', [
+        "title" => "About",
+        "posts" => $blog_post
+    ]);
+});
+
+Route::get('about/{slug}', function ($slug) {
+    $blog_post = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Azhmi Fauzi Mahdin",
+            "body" => "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit voluptatum repudiandae sequi fuga at? Voluptatum ullam voluptates facilis iste, iusto, molestiae quis illo, adipisci rem impedit nostrum saepe hic? Deleniti!"
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => 'judul-post-kedua',
+            "author" => "Airlangga",
+            "body" => "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit voluptatum repudiandae sequi fuga at? Voluptatum ullam voluptates facilis iste, iusto, molestiae quis illo, adipisci rem impedit nostrum saepe hic? Deleniti!"
+        ]
+    ];
+
+    $new_post = [];
+    foreach ($blog_post as $post) {
+        if ($post["slug"] === $slug) {
+            $new_post = $post;
+        }
+    }
+    return view('post', [
+        "title" => "Single Post",
+        "post" => $new_post
     ]);
 });
