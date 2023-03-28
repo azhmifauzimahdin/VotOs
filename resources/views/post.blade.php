2ff454dea01810577e2eb765812bda4b569f106c
Azhmi Fauzi Mahdin <azhmifauzi11@gmail.com>
2023-03-28 14:30:41 +0700

    [8]Post Category & Eloquent Relationship

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,6 +2,7 @@
 
 @section('container')
     <h2>{{ $post->title }}</h2>
+    <p>By. Azhmi Fauzi Mahdin in <a href="/categories/{{ $post->category->slug }}"> {{$post->category->name }}</a></p>
     {!! $post->body !!}
     <a href="/posts">Back to Posts</a>
 @endsection
