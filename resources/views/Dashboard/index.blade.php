3b57abb27afcfd012e082f620e263afe3e90d679
Azhmi Fauzi Mahdin <azhmifauzi11@gmail.com>
2023-04-01 14:01:17 +0700

    [17]Dashboard UI

--- resources/views/Dashboard/index.blade.php
+++ resources/views/Dashboard/index.blade.php
@@ -1,1 +1,8 @@
-<h1>Welcome, Azhmi Fauzi Mahdin</h1>
+@extends('dashboard.layouts.main')
+
+@section('container')
+    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h1 class="h2">Welcome back, {{ auth()->user()->name }}</h1>
+        <span data-feather="log-out"></span>
+    </div>
+@endsection
